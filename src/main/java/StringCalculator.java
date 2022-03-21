5d97651736a7c274c75d605bdf603235550fe7e8
wil <whenao86@gmail.com>
2022-03-21 14:55:30 -0500

    Hacer algo mas de Refactorizacion

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -8,59 +8,77 @@ import java.util.List;
 public class StringCalculator {
     public int add(String values) throws Exception { //"1,1"
         if(values.length() > 0) {
-            int temp;
-            String[] delimitersArr = new String[1];
-            try {
-                temp = Integer.parseInt("" + values.charAt(0));
-                delimitersArr[0] = null;
-            } catch(Exception e) {
-                if(("" + values.charAt(0)) == "-") {
-                    delimitersArr[0] = null;
-                } else if (values.charAt(0) == '['){
-                    String subStringLast = values.substring(0, values.lastIndexOf(']'));
-                    delimitersArr = subStringLast
-                            .replace('[', ' ')
-                            .replace(']', ' ')
-                            .trim()
-                            .replaceAll("\\s+", ";")
-                            .split(";");
-                    Arrays.sort(delimitersArr, Collections.reverseOrder());
-                } else {
-                    delimitersArr[0] = "" + values.charAt(0);
-                }
-            }
 
-            String[] splittedList2 = null;
-            if(delimitersArr.length == 1 && delimitersArr[0] != null && delimitersArr[0].length() == 1) {
-                splittedList2 = values.substring(1, values.length()).split(delimitersArr[0]);
-            } else if (delimitersArr[0] != null && (delimitersArr.length > 1 || delimitersArr[0].length() > 1)){
-                List<String> delimiterList = Arrays.asList(delimitersArr);
-                for (String del: delimiterList){
-                    values = values.replaceAll(del, "_");
-                }
-                splittedList2 = values.substring(values.lastIndexOf(']') + 1, values.length()).split("_");
-            }
-            else {
-                splittedList2 = values.split("[,|\n]");
-            }
+            String[] delimitersArr = buildDelimiterArray(values);
 
-            ArrayList<Integer> numberList = new ArrayList<Integer>();
+            String[] splittedList = generateOperandList(values, delimitersArr);
+
+            ArrayList<Integer> numberList = generateNumberList(splittedList);
+
             int accumulator = 0;
-            for(String element: splittedList2) {
-                int tempValue = Integer.parseInt(element);
-                if(tempValue < 0) {
-                    throw new Exception("NegativeNumberException");
-                }
-                if(tempValue > 1000) {
-                    continue;
-                }
-                numberList.add(tempValue);
-            }
             for(Integer number: numberList) {
                 accumulator += number;
             }
             return accumulator;
         }
         return 0;
+    }
+
+    private ArrayList<Integer> generateNumberList(String[] splittedList) throws Exception{
+        ArrayList<Integer> numberList = new ArrayList<Integer>();
+        for(String element: splittedList) {
+            int tempValue = Integer.parseInt(element);
+            if(tempValue < 0) {
+                throw new Exception("NegativeNumberException");
+            }
+            if(tempValue > 1000) {
+                continue;
+            }
+            numberList.add(tempValue);
+        }
+        return numberList;
+    }
+
+    private String[] generateOperandList(String values, String[] delimitersArr) {
+        String[] splittedList = null;
+        if(delimitersArr.length == 1 && delimitersArr[0] != null && delimitersArr[0].length() == 1) {
+            return splittedList = values.substring(1, values.length()).split(delimitersArr[0]);
+        }
+        if (delimitersArr[0] != null && (delimitersArr.length > 1 || delimitersArr[0].length() > 1)){
+            List<String> delimiterList = Arrays.asList(delimitersArr);
+            for (String del: delimiterList){
+                values = values.replaceAll(del, "_");
+            }
+            return splittedList = values.substring(values.lastIndexOf(']') + 1, values.length()).split("_");
+        }
+        return splittedList = values.split("[,|\n]");
+    }
+
+    private String[] buildDelimiterArray(String values) {
+        int temp;
+        String[] delimitersArr = new String[1];
+        try {
+            temp = Integer.parseInt("" + values.charAt(0));
+            delimitersArr[0] = null;
+            return delimitersArr;
+        } catch(Exception e) {
+            if(("" + values.charAt(0)) == "-") {
+                delimitersArr[0] = null;
+                return delimitersArr;
+            }
+            if (values.charAt(0) == '['){
+                String subStringLast = values.substring(0, values.lastIndexOf(']'));
+                delimitersArr = subStringLast
+                        .replace('[', ' ')
+                        .replace(']', ' ')
+                        .trim()
+                        .replaceAll("\\s+", ";")
+                        .split(";");
+                Arrays.sort(delimitersArr, Collections.reverseOrder());
+                return delimitersArr;
+            }
+            delimitersArr[0] = "" + values.charAt(0);
+        }
+        return delimitersArr;
     }
 }
